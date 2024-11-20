b2fa507e25109c72e328df3ddcce76dbfe6a3496
estefaniaderosa <estefania.derosa@gmail.com>
2024-11-20 14:47:55 +0100

    Code refactor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import TodoList from './components/TodoList'
-import './App.css';
-
-
+import React from "react";
+import TodoList from "./components/TodoList";
+import "./App.css";
 
 const App = () => {
-    return (
-        <>
-            <div className="todo-app">
-                <TodoList />
-            </div>
-            <div className="footer">
-                <p>Made with &hearts; by Estefanía De Rosa Gil</p>
-            </div>
-            </>
-        
-    )
-}
+	return (
+		<>
+			<div className='todo-app'>
+				<TodoList />
+			</div>
+			<div className='footer'>
+				<p>Made with &hearts; by Estefanía De Rosa Gil</p>
+			</div>
+		</>
+	);
+};
 
 export default App;
--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -1,30 +1,28 @@
+import { useState } from "react";
 
-import { useState } from 'react';
+function useLocalStorage(key, initialValue) {
+	const [storedValue, setStoredValue] = useState(() => {
+		try {
+			const item = window.localStorage.getItem(key);
+			return item ? JSON.parse(item) : initialValue;
+		} catch (error) {
+			console.log(error);
+			return initialValue;
+		}
+	});
 
-function useLocalStorage(key, initialValue) { 
-    const [storedValue, setStoredValue] = useState(() => {
-      try {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
-      } catch (error) {
-        console.log(error);
-        return initialValue;
-      }
-    });
-  
-   
-    const setValue = value => {
-      try {
-        const valueToStore =
-          value instanceof Function ? value(storedValue) : value;
-        setStoredValue(valueToStore);
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    return [storedValue, setValue];
-  }
+	const setValue = (value) => {
+		try {
+			const valueToStore =
+				value instanceof Function ? value(storedValue) : value;
+			setStoredValue(valueToStore);
+			window.localStorage.setItem(key, JSON.stringify(valueToStore));
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  export default useLocalStorage;
+	return [storedValue, setValue];
+}
+
+export default useLocalStorage;
